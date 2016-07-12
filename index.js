56005e48761527be110e9f07a4318c0eb37fb5dd
Gina A. Benavidez <gbenavid@my.hpu.edu>
2016-07-11 19:20:19 -1000

    Complete assignment

--- index.js
+++ index.js
@@ -1,147 +1,72 @@
-/* create variables */
+// #1: create varibles using string text/
+var firstName = "Gina";
+var lastName = "Benavidez";
+var birthPlace = "Oregon";
+console.log(firstName);
+console.log(lastName);
+console.log(birthPlace);
 
-/**
-* #1
-* Variables with a String as their value
-*
-* Declare variables named `firstName`, `lastName`, and `birthPlace`.
-* Fill out the values for these with your own data or make it up.
-*
-* It should be a String.
-**/
+//* #2 create Variables with a Number as their value/
+var favoriteNumber = 2016;
+var currentYear = 2016;
+var thatOnePrinceSong = 1999;
+console.log(favoriteNumber);
+console.log(currentYear);
+console.log(thatOnePrinceSong);
 
+//* #3 Variables with Boolean values//
+var isDaytime = false;
+var isLeftHanded = false;
+var inHawaii = true;
+var isHappyCoding = true;
+console.log(isDaytime);
+console.log(isLeftHanded);
+console.log(inHawaii);
 
+//** #4 Variables with null/
+var enrolledAtDevLeague = null;
+var completedFridayPrep = null;
+console.log(enrolledAtDevLeague);
+console.log(completedFridayPrep);
 
-/**
-* #2
-* Variables with a Number as their value
-*
-* Declare variables named `favoriteNumber`, `currentYear`,
-* and `thatOnePrinceSong`.
-*
-* Their values should be a Number
-**/
+//** #5a Variables with an Array for it's value./
+var westCoast = ["WA","OR","CA", "HI"];
+console.log(westCoast);
+//** #5b Variables with an Array for it's value. 
+var evenNumbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+console.log(evenNumbers);
 
+//** #6a Arithmetic Variables with the outcome of an arithmetic operation/
+var sumOfNumbers = 5+5+5;
+var subtractionOfNumbers = 10-1;
+var multiplicationOfNumbers = 3*3;
+var divisionOfNumbers = 9/3;
+console.log(sumOfNumbers);
+console.log (subtractionOfNumbers);
+console.log(multiplicationOfNumbers);
+console.log(divisionOfNumbers);
+//** #6b Comparisons & Logic Variables with the outcome of a comparison operation:/
+var isEqual = "Tacocat" == "tacocat";
+console.log(isEqual)//false.
+//* BONUS: See if you can come up with a few examples of double comparison operator(==) vs triple comparison operator(===)./
+var CompareMe = "5" === 5;
+var CompareMeToo = "five" == "5"; 
+console.log(CompareMe); // false.
+console.log(CompareMeToo); // false.
+//* #6c Assignments Variables with the outcome of an assignment operation:
+var generalContainer = ["starfruit", "lychee", "dragon fruit", "mango", "papayas"];
+console.log(generalContainer [4]);
 
-
-/**
-* #3
-* Variables with Boolean values
-*
-* Declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`,
-* and `isHappyCoding`
-*
-* Their values should be a Boolean
-**/
-
-
-
-/**
-* #4
-* Variables with null
-*
-* Declare variables named `enrolledAtDevLeague` and `completedFridayPrep`
-*
-* Their values should be a Null value
-**/
-
-
-
-/**
-* #5a
-* Variables with an Array for it's value. This Array will contain:
-*
-* Declare a variable named `westCoast` which is an Array.
-* This Array contains the names of all the states
-* which can be found along the west coast of The United States.
-**/
-
-
-
-/**
-* #5b
-* Variables with an Array for it's value. This Array will contain:
-*
-* Declare a variable named `evenNumbers` which is an Array.
-* This Array contains numbers that are considered 'even values'.
-* Have at least ten even values in this array.
-**/
-
-
-
-/**
-* #6a Arithmetic
-* Variables with the outcome of an arithmetic operation:
-*
-* Declare a variable named `sumOfNumbers` which is a Number. This Number
-* contains the result of performing an arithmetic operation on two numbers.
-* Have this variable store the result of adding 3 numbers of your choosing.
-*
-* Extend this behavior to include subtraction, multiplication and division.
-*/
-
-
-
-
-/**
-* #6b Comparisons & Logic
-* Variables with the outcome of a comparison operation:
-*
-* Declare a variable named `isEqual` which is a Boolean. This Boolean
-* contains the result of performing a comparison to test if two strings
-* are equal. We want to compare the two strings "Tacocat" and "tacocat" and
-* `console.log()` the result.
-*
-* NOTE:
-*   There is no test which tests your console.log()'s output'.
-*   Also, there are zero tests for the BONUS below.
-*
-* BONUS:
-*   See if you can come up with a few examples of double comparison
-*   operator(==) vs triple comparison operator(===).
-*/
-
-
-
-/**
-* #6c Assignments
-* Variables with the outcome of an assignment operation:
-*
-* Declare a variable named `generalContainer`. This variable will be used to store multiple types of
-* values as we assign them to show how variables in JavaScript are not bound to a single data type
-* and can change values.
-*
-* Write 5 different assignment statements and `console.log()` the value of the variable after each
-* assignment statement to show the current value.
-*
-* NOTE:
-*   The tests for this section cannot test your variable as it changes,
-*   it can only test that you declared the variable by giving it the right name.
-*/
-
-
-
-/**
-* #7a
-* Declare Functions
-*
-* Declare a Function named `jump` which takes a single argument, `height`
-*
-* This function should return a String, "You jumped 9 feet high!"
-* if invoked in this way -> jump(9);
-*
-* Store the return value to a variable named `jumpResult` and use console.log to inspect the value which was returned by your function
-**/
-
-
-
-/**
-* #7b
-* Declare Functions
-*
-* Declare a Function named `cook` which a takes three arguments/parameters, `ingredient1`, `ingredient2`, `recipeName`
-*
-* This function should return a String along the lines of, "Tomatoes and Cheese make a Pizza!"
-* if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
-**/
-
+//* #7a Declare Functions
+function jump (height) {
+	console.log("You jumped " + height + " feet high!");
+	return "You jumped " + height + " feet high!";
+}
+jump(9);
+console.log(jump(9));
+//* #7b
+function cook(ingredient1, ingredient2, recipeName){
+	return ingredient1 + " and " + ingredient2 + " make a " + recipeName + "!";
+}
+var dinner = cook("natto", "uni", "pizza");
+console.log(dinner);
